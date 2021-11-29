d2034fdd3b2d79693ae2422c6dc8b94c3060f278
rtm.pplpst <rtm.pplpst@gmail.com>
2021-11-28 23:42:03 -0500

    update readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Gifish - The language of speaking in GIFs.
+
+## Installation and running
+
+```sh
+yarn install && yarn build && yarn start
+```
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,23 @@
 import { Box, Flex } from "@chakra-ui/react";
+import { Helmet } from "react-helmet-async";
 
 import Gifish from "components/Gifish";
 import SearchBar from "components/SearchBar";
 
 const Home = () => (
-  <main>
-    <Flex direction="column" alignItems="center" w="100%" h="100%" marginTop={16}>
-      <Box mb={8}>
-        <Gifish width="256px" text />
-      </Box>
-      <SearchBar />
-    </Flex>
-  </main>
+  <>
+    <Helmet>
+      <title>{process.env.REACT_APP_WEBSITE_NAME}</title>
+    </Helmet>
+    <main>
+      <Flex direction="column" alignItems="center" w="100%" h="100%" marginTop={16}>
+        <Box mb={8}>
+          <Gifish width="256px" text />
+        </Box>
+        <SearchBar />
+      </Flex>
+    </main>
+  </>
 );
 
 export default Home;
